4dfdcef072f863eb22fdfeafe577578f470c969b
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2024-07-01 21:54:40 +0200

    création du fichier scrollEvent contenant tous les animations du site

---
+++ public/javascripts/scrollEvent.js
@@ -0,0 +1,34 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+ScrollReveal().reveal(".header__content h2", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".header__content .header__btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+ScrollReveal().reveal(".header__content .socials", {
+    ...scrollRevealOption,
+    delay: 2500,
+});
+ScrollReveal().reveal(".header__bar", {
+    ...scrollRevealOption,
+    delay: 3000,
+});
