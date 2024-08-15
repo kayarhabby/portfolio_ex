eb4ad3d702d40a5b73015d81b696f052077bf0ea
kayarhabby <pasciahershekaya@gmail.com>
2024-08-16 00:46:00 +0200

    update : substition de require par import

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+import express from 'express';
 var router = express.Router();
 
 /* GET home page. */
@@ -23,4 +23,4 @@ router.get('/projectDesc', function(req, res, next) {
   res.render('projectDesc', { title: 'Express' });
 });
 
-module.exports = router;
+export default router;
