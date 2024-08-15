949ebfe8d2fc4417615c8124802ae0d3fdf22c2b
kayarhabby <pasciahershekaya@gmail.com>
2024-08-16 00:48:19 +0200

    update : substition de require par import

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import createError from 'http-errors';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import projectsRouter from './routes/projects.js';
+import skillsRouter from './routes/skills.js';
+import usersRouter from './routes/users.js';
+import bootcampRouter from './routes/bootcamp.js';
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(path.dirname(new URL(import.meta.url).pathname), 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(path.dirname(new URL(import.meta.url).pathname), 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', projectsRouter);
+app.use('/api', skillsRouter);
+app.use('/api', usersRouter);
+app.use('/api', bootcampRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +43,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
