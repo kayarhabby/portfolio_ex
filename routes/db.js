7649291606943189d1697b4ff0f789615363c6f6
kayarhabby <pasciahershekaya@gmail.com>
2024-08-21 12:16:45 +0200

    mise en place des variables d'environnements

--- routes/db.js
+++ routes/db.js
@@ -1,11 +1,13 @@
 import pg from 'pg';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const pool = new pg.Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "PortfolioDB",
-    password: "Pascia1877!",
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
 
 export default pool;
