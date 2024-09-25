d18df1beb3d83e147c7bf77874a504d9c09a1f1c
kayarhabby <pasciahershekaya@gmail.com>
2024-09-25 22:40:32 +0200

    vercel

--- routes/db.js
+++ routes/db.js
@@ -2,12 +2,8 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const pool = new pg.Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-});
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+})
 
 export default pool;
